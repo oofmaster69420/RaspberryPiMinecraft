1886b261e433acbf85d7247e9c1ca6321c819b34
oofmaster69420 <57970114+oofmaster69420@users.noreply.github.com>
2021-01-05 19:52:37 -0500

    Update start_lowspec.sh

--- start_lowspec.sh
+++ start_lowspec.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # James Chambers - V1.0 - March 24th 2018
 # Marc Tönsing - V1.2 - September 16th 2019
+# Modified by Joey Reinhart
 # Minecraft Server low spec startup script using screen
 echo "Starting Minecraft server.  To view window type screen -r minecraft."
 echo "To minimize the window and let the server run in the background, press Ctrl+A then Ctrl+D"
