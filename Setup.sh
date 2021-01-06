abfa53ac1b4f2676f5fbfa63e83cd0caffd53f39
oofmaster69420 <57970114+oofmaster69420@users.noreply.github.com>
2021-01-05 20:05:38 -0500

    Update and rename SetupMinecraft.sh to Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # Original Minecraft Server Installation Script - James A. Chambers - https://www.jamesachambers.com.
 # Changes and simplifications by Marc Tönsing
-# V1.16.1 05.07.20
-# GitHub Repository: https://github.com/mtoensing/RaspberryPiMinecraft
+# Modified by Joey Reinhart
 
 echo "Minecraft Server installation script by James Chambers and Marc Tönsing - V1.0"
 echo "Latest version always at https://github.com/mtoensing/RaspberryPiMinecraft"
@@ -15,7 +14,7 @@ fi
 echo "Updating packages..."
 sudo apt-get update
 
-echo "Installing latest Java OpenJDK 11..."
+echo "Installing Java..."
 sudo apt-get install openjdk-11-jre-headless -y
 
 echo "Installing screen... "
@@ -25,28 +24,34 @@ echo "Creating minecraft server directory..."
 mkdir minecraft
 cd minecraft
 
-echo "Getting latest Paper Minecraft server..."
-wget -O paperclip.jar https://papermc.io/api/v1/paper/1.16.4/latest/download
+echo "Getting latest Spigot Minecraft server..."
+wget -O server.jar https://cdn.getbukkit.org/spigot/spigot-1.16.4.jar
 
 echo "Building the Minecraft server... "
-java -jar -Xms800M -Xmx800M paperclip.jar
+java -jar -Xms800M -Xmx800M server.jar
 
 echo "Accepting the EULA... "
 echo eula=true > eula.txt
 
-echo "Grabbing start.sh from repository... "
-wget -O start.sh https://raw.githubusercontent.com/mtoensing/RaspberryPiMinecraft/master/start.sh
-chmod +x start.sh
+echo "Grabbing run.sh from repository... "
+wget -O run.sh https://raw.githubusercontent.com/oofmaster69420/RaspberryPiMinecraft/master/run.sh
+chmod +x run.sh
 
-echo "Oh wait. Checking for total memory available..."
 TotalMemory=$(awk '/MemTotal/ { printf "%.0f\n", $2/1024 }' /proc/meminfo)
-if [ $TotalMemory -lt 3000 ]; then
-  echo "Sorry, have to grab low spec start.sh from repository... "
-  wget -O start.sh https://raw.githubusercontent.com/mtoensing/RaspberryPiMinecraft/master/start_lowspec.sh
+if [ $TotalMemory -lt 6000 ]; then
+  rm run.sh
+  wget -O run.sh https://raw.githubusercontent.com/oofmaster69420/RaspberryPiMinecraft/master/run_lowspec.sh
+  chmod +x run.sh
+fi
+
+if [ $TotalMemory -lt 2500 ]; then
+  rm run.sh
+  wget -O run.sh https://raw.githubusercontent.com/oofmaster69420/RaspberryPiMinecraft/master/run_superlowspec.sh
+  chmod +x run.sh
 fi
 
 echo "Grabbing restart.sh from repository... "
-wget -O restart.sh https://raw.githubusercontent.com/mtoensing/RaspberryPiMinecraft/master/restart.sh
+wget -O restart.sh https://raw.githubusercontent.com/oofmaster69420/RaspberryPiMinecraft/master/restart.sh
 chmod +x restart.sh
 
 echo "Enter a name for your server "
@@ -54,5 +59,5 @@ read -p 'Server Name: ' servername
 echo "server-name=$servername" >> server.properties
 echo "motd=$servername" >> server.properties
 
-echo "Setup is complete. To run the server go to the minecraft directory and type ./start.sh"
+echo "Setup is complete. To run the server go to the minecraft directory and type ./run.sh"
 echo "Don't forget to set up port forwarding on your router. The default port is 25565"
