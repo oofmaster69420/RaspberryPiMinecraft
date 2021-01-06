8aa80f2f9546b0c3b86a6799a57e03bff4a64748
oofmaster69420 <57970114+oofmaster69420@users.noreply.github.com>
2021-01-05 20:12:51 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # James Chambers - V1.0 - March 24th 2018
 # Marc Tönsing - V1.2 - September 16th 2019
-# Modified by Joey Reinhart
+# Modified by Joey Reinhart on Jan 5th 2021
 # Minecraft Server startup script using screen
 echo "Starting Minecraft server.  To view window type screen -r minecraft."
 echo "To minimize the window and let the server run in the background, press Ctrl+A then Ctrl+D"
