be85f6d0ce872983e68852321bda0ae906f71c12
oofmaster69420 <57970114+oofmaster69420@users.noreply.github.com>
2021-01-05 20:43:45 -0500

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -2,6 +2,7 @@
 # Marc Tönsing - V1.1 - 25.08.2020
 # Modified by Joey Reinhart on Jan 5th 2021
 # Minecraft Server restart & update and pi reboot.
+user=$(whoami)
 screen -Rd minecraft -X stuff "say Server is restarting in 5 seconds! $(printf '\r')"
 sleep 1s
 screen -Rd minecraft -X stuff "say Server is restarting in 4 seconds! $(printf '\r')"
@@ -16,6 +17,6 @@ screen -Rd minecraft -X stuff "say Closing server...$(printf '\r')"
 screen -Rd minecraft -X stuff "stop $(printf '\r')"
 sleep 15s
 echo "Updating to most recent Spigot version."
-wget -q -O /home/pi/minecraft/server.jar https://cdn.getbukkit.org/spigot/spigot-1.16.4.jar
+wget -q -O /home/$user/minecraft/server.jar https://cdn.getbukkit.org/spigot/spigot-1.16.4.jar
 echo "Restarting now."
-sudo /sbin/reboot
+sudo reboot
