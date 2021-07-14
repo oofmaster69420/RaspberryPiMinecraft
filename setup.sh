cac6ec8d44ef4109e97b6c1505df9e28d025c51c
oofmaster69420 <57970114+oofmaster69420@users.noreply.github.com>
2021-07-14 13:28:48 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,11 @@
 echo "Minecraft Server installation script by James Chambers and Marc Tönsing - V1.0"
 echo "Modified by Joey Reinhart"
 echo "Latest version always at https://github.com/oofmaster69420/RaspberryPiMinecraftServer"
+echo "This script automaticly agrees to the Minecraft EULA for you"
+echo "If you don't want to agree to the EULA press Ctrl+C now or wait 5 secconds for the script to start"
+echo "You can read the EULA at https://www.minecraft.net/en-us/eula"
+
+sleep 5
 
 if [ -d "minecraft" ]; then
   echo "Directory minecraft already exists!  Exiting... "
@@ -16,7 +21,7 @@ echo "Updating packages..."
 sudo apt-get update
 
 echo "Installing Java..."
-sudo apt-get install openjdk-11-jre-headless -y
+sudo apt-get install openjdk-16-jre-headless -y
 
 echo "Installing screen... "
 sudo apt-get install screen -y
@@ -25,35 +30,30 @@ echo "Creating minecraft server directory..."
 mkdir minecraft
 cd minecraft
 
-echo "Getting latest Spigot Minecraft server..."
-wget -O server.jar https://cdn.getbukkit.org/spigot/spigot-1.16.5.jar
+echo "Getting latest Paper Minecraft server..."
+wget -O server.jar https://papermc.io/api/v2/projects/paper/versions/1.17.1/builds/104/downloads/paper-1.17.1-104.jar
 
 echo "Building the Minecraft server... "
-java -jar -Xms800M -Xmx800M server.jar
+java -jar server.jar
 
 echo "Accepting the EULA... "
 echo eula=true > eula.txt
 
 echo "Grabbing run.sh from repository... "
-wget -O run.sh https://raw.githubusercontent.com/oofmaster69420/RaspberryPiMinecraftServer/master/run.sh
-chmod +x run.sh
+wget -O run.sh https://raw.githubusercontent.com/oofmaster69420/RaspberryPiMinecraftServer/master/run-8GB.sh
 
 TotalMemory=$(awk '/MemTotal/ { printf "%.0f\n", $2/1024 }' /proc/meminfo)
 if [ $TotalMemory -lt 6000 ]; then
   rm run.sh
-  wget -O run.sh https://raw.githubusercontent.com/oofmaster69420/RaspberryPiMinecraftServer/master/run_lowspec.sh
-  chmod +x run.sh
+  wget -O run.sh https://raw.githubusercontent.com/oofmaster69420/RaspberryPiMinecraftServer/master/run-4GB.sh
 fi
 
 if [ $TotalMemory -lt 2500 ]; then
   rm run.sh
-  wget -O run.sh https://raw.githubusercontent.com/oofmaster69420/RaspberryPiMinecraftServer/master/run_superlowspec.sh
-  chmod +x run.sh
+  wget -O run.sh https://raw.githubusercontent.com/oofmaster69420/RaspberryPiMinecraftServer/master/run-2GB.sh
 fi
 
-echo "Grabbing restart.sh from repository... "
-wget -O restart.sh https://raw.githubusercontent.com/oofmaster69420/RaspberryPiMinecraftServer/master/restart.sh
-chmod +x restart.sh
+chmod +x run.sh
 
 echo "Setup is complete. To run the server go to the minecraft directory and type ./run.sh"
 echo "Don't forget to set up port forwarding on your router. The default port is 25565"
