0313d69a21a390b11a00330c4c945658fe037aa7
oofmaster69420 <57970114+oofmaster69420@users.noreply.github.com>
2021-07-14 13:17:31 -0400

    Update run_superlowspec.sh
    Removed EULA agreement

--- run_superlowspec.sh
+++ run_superlowspec.sh
@@ -7,4 +7,4 @@ user=$(whoami)
 echo "Starting Minecraft server.  To view window type screen -r minecraft."
 echo "To minimize the window and let the server run in the background, press Ctrl+A then Ctrl+D"
 cd /home/$user/minecraft/
-/usr/bin/screen -dmS minecraft /usr/bin/java -jar -Xms1200M -Xmx1200M -Dcom.mojang.eula.agree=true /home/$user/minecraft/server.jar
+/usr/bin/screen -dmS minecraft /usr/bin/java -jar -Xms1200M -Xmx1200M /home/$user/minecraft/server.jar
